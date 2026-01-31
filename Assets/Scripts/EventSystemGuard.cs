5fe4fe4861b1912a794b7a49f3dff3dd78cb85f5
Esenciak <karol.jablonski00@gmail.com>
2026-01-31 11:56:59 +0100

    Dodaje EventGuard i do debugowania coordy

--- Assets/Scripts/EventSystemGuard.cs
+++ Assets/Scripts/EventSystemGuard.cs
@@ -2,41 +2,92 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
+#if ENABLE_INPUT_SYSTEM && !ENABLE_LEGACY_INPUT_MANAGER
+using UnityEngine.InputSystem.UI;
+#endif
+
 public class EventSystemGuard : MonoBehaviour
 {
+	private static EventSystemGuard _instance;
+
 	private void Awake()
 	{
-		SceneManager.sceneLoaded += (_, __) => Cleanup();
-		Cleanup();
+		if (_instance != null && _instance != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		_instance = this;
+		DontDestroyOnLoad(gameObject);
+
+		SceneManager.sceneLoaded += OnSceneLoaded;
+
+		EnsureSingleEventSystem();
 	}
 
 	private void OnDestroy()
 	{
-		SceneManager.sceneLoaded -= (_, __) => Cleanup();
+		if (_instance == this)
+		{
+			SceneManager.sceneLoaded -= OnSceneLoaded;
+		}
 	}
 
-	private void Cleanup()
+	private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
 	{
-		var systems = FindObjectsOfType<EventSystem>(true);
-		if (systems == null || systems.Length <= 1) return;
+		EnsureSingleEventSystem();
+	}
 
-		// Preferuj EventSystem z aktywnej sceny
-		EventSystem keep = null;
-		var active = SceneManager.GetActiveScene();
-		foreach (var es in systems)
-		{
-			if (es != null && es.gameObject.scene == active)
-			{
-				keep = es;
-				break;
-			}
-		}
-		if (keep == null) keep = systems[0];
+	private static void EnsureSingleEventSystem()
+	{
+		var systems = Object.FindObjectsOfType<EventSystem>(true);
 
-		foreach (var es in systems)
+		if (systems == null || systems.Length == 0)
 		{
-			if (es != null && es != keep)
-				Destroy(es.gameObject);
+			CreateEventSystem();
+			return;
 		}
+
+		// zostaw pierwszy, usuñ resztê
+		var keep = systems[0];
+		for (int i = 1; i < systems.Length; i++)
+		{
+			if (systems[i] != null)
+				Object.Destroy(systems[i].gameObject);
+		}
+
+		if (!keep.gameObject.activeInHierarchy) keep.gameObject.SetActive(true);
+		if (!keep.enabled) keep.enabled = true;
+
+		EnsureCorrectInputModule(keep.gameObject);
+	}
+
+	private static void CreateEventSystem()
+	{
+		var go = new GameObject("EventSystem");
+		go.AddComponent<EventSystem>();
+		EnsureCorrectInputModule(go);
+		Object.DontDestroyOnLoad(go);
+	}
+
+	private static void EnsureCorrectInputModule(GameObject eventSystemGO)
+	{
+#if ENABLE_INPUT_SYSTEM && !ENABLE_LEGACY_INPUT_MANAGER
+        // New Input System only
+        var legacy = eventSystemGO.GetComponent<StandaloneInputModule>();
+        if (legacy != null) Object.Destroy(legacy);
+
+        if (eventSystemGO.GetComponent<InputSystemUIInputModule>() == null)
+            eventSystemGO.AddComponent<InputSystemUIInputModule>();
+#else
+		// Legacy (albo Both) - zapewnij StandaloneInputModule
+		// Usuñ InputSystemUIInputModule jeœli ktoœ go przyniós³ prefabem/scen¹ (bez twardej referencji)
+		var inputSystemModule = eventSystemGO.GetComponent("UnityEngine.InputSystem.UI.InputSystemUIInputModule");
+		if (inputSystemModule != null) Object.Destroy(inputSystemModule);
+
+		if (eventSystemGO.GetComponent<StandaloneInputModule>() == null)
+			eventSystemGO.AddComponent<StandaloneInputModule>();
+#endif
 	}
 }
